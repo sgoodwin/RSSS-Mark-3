6d320520d944eec84d7656a5eccc73e6693a743f
Samuel Goodwin <samuel.goodwin@me.com>
2012-02-19 15:48:51 -0500

    Test to ensure users don't see each other's feeds.

--- test/user-auth-test.js
+++ test/user-auth-test.js
@@ -2,6 +2,7 @@ var vows = require('vows'),
 	assert = require('assert'),
 	helper = require('./helper.js'),
 	index = require('../index.js'),
+	async = require('async'),
 	exampleFeed = {'title':'Labnotes', 'type':'rss', 'htmlUrl':'http://labnotes.org/', 'xmlUrl':'http://labnotes.org/feed/atom/'};
 
 /*
@@ -14,7 +15,7 @@ A user should be able to get summary info about their account?
 */
 
 vows.describe('User authentication').addBatch({
-	'When as ask for all the feeds in JSON without authentication': {
+	'When a user asks for all the feeds in JSON without authentication': {
 		topic: function () {
 			helper.get({'accept':'application/json'}, '/feeds', this.callback);
 		},
@@ -22,7 +23,7 @@ vows.describe('User authentication').addBatch({
 			assert.equal(topic.statusCode, 401);
 		}
 	},
-	'When we try to GET the feedlist with a valid username and password': {
+	'When a user tries to GET the feedlist with a valid username and password': {
 		topic: function () {
 			helper.get({'accept':'application/json', 'authorization':helper.basicAuth('sgoodwin', 'poop')}, '/feeds', this.callback);
 		},
@@ -30,5 +31,32 @@ vows.describe('User authentication').addBatch({
 			assert.isObject(topic.body);
 			assert.isArray(topic.body.feeds);
 		}
-	}
+	},
+	'When a user tries to submit a new feed without authentication': {
+		topic: function () {
+			var exampleFeed = {'title':'Labnotes', 'type':'rss', 'htmlUrl':'http://labnotes.org/', 'xmlUrl':'http://labnotes.org/feed/atom/'};
+			helper.post({'accept':'application/json'}, '/feeds', {'feed':exampleFeed}, this.callback);
+		},
+		'we get an authentication error': function (topic) {
+			assert.equal(topic.statusCode, 401);
+		}
+	},
+	'When a user submits a feed with aunthentication': {
+		topic: function () {
+			var exampleFeed = {'title':'Labnotes', 'type':'rss', 'htmlUrl':'http://labnotes.org/', 'xmlUrl':'http://labnotes.org/feed/atom/'},
+				headers = {'accept':'application/json', 'authentication': helper.basicAuth('sgoodwin', 'poop')};
+				
+			helper.post(headers, '/feeds', {'feed':exampleFeed}, this.callback);
+		},
+		'a different user should not see that feed in their list': function (topic) {
+			var otherUserHeaders = {'accept':'application/json','authentication': helper.basicAuth('kisom', 'poop')};
+			helper.get(otherUserHeaders, '/feeds', function(err, response){
+				async.some(response.body.feeds, function(feed, truthCallback){
+					return feed === topic.body.feed;
+				}, function(result){
+					assert.isFalse(result);
+				});
+			});
+		}
+	},
 }).export(module);
