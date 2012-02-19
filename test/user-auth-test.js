7e49402ec558f050c1e7eecd28b258d8e376a2aa
Samuel Goodwin <samuel.goodwin@me.com>
2012-02-19 16:21:50 -0500

    Had to re-write the tests when I realized how vows works. They pass now properly.

--- test/user-auth-test.js
+++ test/user-auth-test.js
@@ -3,11 +3,11 @@ var vows = require('vows'),
 	helper = require('./helper.js'),
 	index = require('../index.js'),
 	async = require('async'),
+	userBHeaders = {'accept':'application/json','authorization': helper.basicAuth('kisom', 'poop')},
+	userAHeaders = {'accept':'application/json', 'authorization':helper.basicAuth('sgoodwin', 'poop')},
 	exampleFeed = {'title':'Labnotes', 'type':'rss', 'htmlUrl':'http://labnotes.org/', 'xmlUrl':'http://labnotes.org/feed/atom/'};
 
 /*
-
-A user should be able to submit feeds and feed items with thir API key
 
 A user should only see feeds and feed item associated with their account
 
@@ -25,7 +25,7 @@ vows.describe('User authentication').addBatch({
 	},
 	'When a user tries to GET the feedlist with a valid username and password': {
 		topic: function () {
-			helper.get({'accept':'application/json', 'authorization':helper.basicAuth('sgoodwin', 'poop')}, '/feeds', this.callback);
+			helper.get(userAHeaders, '/feeds', this.callback);
 		},
 		'we get a list of feeds': function (topic) {
 			assert.isObject(topic.body);
@@ -44,19 +44,34 @@ vows.describe('User authentication').addBatch({
 	'When a user submits a feed with aunthentication': {
 		topic: function () {
 			var exampleFeed = {'title':'Labnotes', 'type':'rss', 'htmlUrl':'http://labnotes.org/', 'xmlUrl':'http://labnotes.org/feed/atom/'},
-				headers = {'accept':'application/json', 'authentication': helper.basicAuth('sgoodwin', 'poop')};
+				topicCallback = this.callback;
 				
-			helper.post(headers, '/feeds', {'feed':exampleFeed}, this.callback);
-		},
-		'a different user should not see that feed in their list': function (topic) {
-			var otherUserHeaders = {'accept':'application/json','authentication': helper.basicAuth('kisom', 'poop')};
-			helper.get(otherUserHeaders, '/feeds', function(err, response){
-				async.some(response.body.feeds, function(feed, truthCallback){
-					return feed === topic.body.feed;
-				}, function(result){
-					assert.isFalse(result);
+			helper.post(userAHeaders, '/feeds', {'feed':exampleFeed}, function(err, responseA){
+				helper.get(userBHeaders, '/feeds', function(err, responseB){
+					async.any(responseB.body.feeds, function(feed, truthCallback){
+						truthCallback(feed === responseA.body.feed);
+					}, function(result){
+						topicCallback(null, result);
+					});
 				});
 			});
+		},
+		'a different user should not see that feed in their list': function (topic) {
+			assert.isFalse(topic);
 		}
 	},
+	'When user A gets their feed list' : {
+		topic: function (){
+			var topicCallback = this.callback;
+			var feedsToCompare = {};
+			helper.get(userAHeaders, '/feeds', function(err, responseA){
+				helper.get(userBHeaders, '/feeds', function(err, responseB){
+					topicCallback(null, [responseA.body.feeds, responseB.body.feeds]);
+				})
+			});
+		},
+		'User B should not have the same feed list': function (topic) {
+			assert.notDeepEqual(topic[0], topic[1]);
+		}
+	}
 }).export(module);
